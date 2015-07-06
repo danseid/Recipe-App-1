f4aed25bb3ef1d54cd1daf1749b7f8862cb0aaf3
hlynursmari1 <hs4011@ic.ac.uk>
2015-07-06 16:08:14 +0100

    Initial Commit

---
+++ Recipe-App-1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Recipe-App-1
+//
+//  Created by Hlynur Sveinbjörnsson on 06/07/2015.
+//  Copyright (c) 2015 Hlynur Sveinbjörnsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Recipe-App-1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Recipe-App-1
+//
+//  Created by Hlynur Sveinbjörnsson on 06/07/2015.
+//  Copyright (c) 2015 Hlynur Sveinbjörnsson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
